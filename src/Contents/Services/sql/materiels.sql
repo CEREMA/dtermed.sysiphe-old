fae54e972c7499e53b2cbc9e02c64f58d78b834b
Stéphane ZUCATTI <stephane.zucatti@me.com>
2017-03-23 15:23:32 +0100

    Update# 127

--- src/Contents/Services/sql/materiels.sql
+++ src/Contents/Services/sql/materiels.sql
@@ -7,9 +7,9 @@ FROM
     right join modeles on materiels.IDMODELE=modeles.IDMODELE
     right join marques on marques.IDMARQUE=modeles.IDMARQUE
     right join familles on familles.IDFAMILLE=modeles.IDFAMILLE
-	right join bpclight_agents on bpclight_agents.kage=affectations.IDUTILISATEUR
+/*	right join bpclight_agents on bpclight_agents.kage=affectations.IDUTILISATEUR*/
     right join utilisateurs on utilisateurs.IDUTILISATEUR=affectations.IDUTILISATEUR
-	right join bpclight_unites on bpclight_agents.kuni=bpclight_unites.kuni
+/*	right join bpclight_unites on bpclight_agents.kuni=bpclight_unites.kuni*/
     right join unites on unites.IDUNITE=utilisateurs.IDUNITE
-	right join bpclight_subdis on bpclight_agents.ksub=bpclight_subdis.ksub
-	right join bpclight_etablissements on bpclight_etablissements.kets=bpclight_unites.kets
+/*	right join bpclight_subdis on bpclight_agents.ksub=bpclight_subdis.ksub
+	right join bpclight_etablissements on bpclight_etablissements.kets=bpclight_unites.kets*/
