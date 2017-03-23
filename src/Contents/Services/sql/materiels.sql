9bc433ef9d97bcf07e5ef139febf7847dfad8b50
Stéphane ZUCATTI <stephane.zucatti@me.com>
2017-03-23 15:49:45 +0100

    Update# 141

--- src/Contents/Services/sql/materiels.sql
+++ src/Contents/Services/sql/materiels.sql
@@ -8,8 +8,8 @@ FROM
     right join marques on marques.IDMARQUE=modeles.IDMARQUE
     right join familles on familles.IDFAMILLE=modeles.IDFAMILLE
 	right join bpclight_agents on bpclight_agents.kage=affectations.IDUTILISATEUR
-    right join utilisateurs on utilisateurs.IDUTILISATEUR=affectations.IDUTILISATEUR
-	right join bpclight_unites on bpclight_agents.kuni=bpclight_unites.kuni
-    right join unites on unites.IDUNITE=utilisateurs.IDUNITE
-	right join bpclight_subdis on bpclight_agents.ksub=bpclight_subdis.ksub
-	right join bpclight_etablissements on bpclight_etablissements.kets=bpclight_unites.kets
+    left join utilisateurs on utilisateurs.IDUTILISATEUR=affectations.IDUTILISATEUR
+	left join bpclight_unites on bpclight_agents.kuni=bpclight_unites.kuni
+    left join unites on unites.IDUNITE=utilisateurs.IDUNITE
+	left join bpclight_subdis on bpclight_agents.ksub=bpclight_subdis.ksub
+	left join bpclight_etablissements on bpclight_etablissements.kets=bpclight_unites.kets
