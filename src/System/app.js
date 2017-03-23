8b85399d52e9438c0d08376911fc05e1a0d4a657
Stéphane ZUCATTI <stephane.zucatti@me.com>
2017-03-23 10:45:28 +0100

    Update# 100

--- src/System/app.js
+++ src/System/app.js
@@ -1,6 +1,33 @@
 App = {
 	init: function(app,server) {
-		app.use('/tmp',server.static(__dirname + require('path').sep+'tmp'));		
+		app.use('/tmp',server.static(__dirname + require('path').sep+'tmp'));
+		app.post('/agents',function(req,res) {
+			res.header("Content-Type", "application/json; charset=utf-8");
+			if (req.body.quest) {
+				var o=JSON.parse(req.body.quest);
+				var db=App.using('db');
+				var objs=[];
+				var where=[];
+				/*objs.push("batiments.LibBatC");
+				objs.push("batiments.GPS");
+				objs.push("agents.*");*/
+				for (var i=0;i<o.length;i++)
+				{
+					var str="";
+					if (i!=0) {
+						str=' '+o[i].operator+' ';
+					};
+					str+=o[i].name;
+					str+=o[i].value;
+					where.push(str);
+				};		
+				var sql=db.get('sysiphe',objs,where);
+				db.model('sysiphe', sql ,function(err,result) {
+					res.end(JSON.stringify(result,null,4));
+				});			
+				return;
+			};
+		});
 	}
 };
 
