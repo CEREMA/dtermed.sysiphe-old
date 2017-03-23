ae433e265f13b6405f078322fea67fdccbb9931c
Stéphane ZUCATTI <stephane.zucatti@me.com>
2017-03-23 16:14:05 +0100

    Update# 151

--- src/Contents/Services/sql/materiels.sql
+++ src/Contents/Services/sql/materiels.sql
@@ -1,4 +1,4 @@
-SELECT
+SELECT DISTINCT
 	*
 FROM 
 	affectations 
@@ -7,9 +7,9 @@ FROM
     right join modeles on materiels.IDMODELE=modeles.IDMODELE
     right join marques on marques.IDMARQUE=modeles.IDMARQUE
     right join familles on familles.IDFAMILLE=modeles.IDFAMILLE
-	left join bpclight_agents on bpclight_agents.kage=affectations.IDUTILISATEUR
+	/*left join bpclight_agents on bpclight_agents.kage=affectations.IDUTILISATEUR*/
     left join utilisateurs on utilisateurs.IDUTILISATEUR=affectations.IDUTILISATEUR
-	left join bpclight_unites on bpclight_agents.kuni=bpclight_unites.kuni
+	/*left join bpclight_unites on bpclight_agents.kuni=bpclight_unites.kuni*/
     left join unites on unites.IDUNITE=utilisateurs.IDUNITE
-	left join bpclight_subdis on bpclight_agents.ksub=bpclight_subdis.ksub
-	left join bpclight_etablissements on bpclight_etablissements.kets=bpclight_unites.kets
+	/*left join bpclight_subdis on bpclight_agents.ksub=bpclight_subdis.ksub
+	left join bpclight_etablissements on bpclight_etablissements.kets=bpclight_unites.kets*/
