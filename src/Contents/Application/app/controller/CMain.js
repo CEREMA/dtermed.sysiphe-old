f6f7d6c5c3063b5e708b65ab581a007cfe0050f2
Stéphane ZUCATTI <stephane.zucatti@me.com>
2017-03-24 14:50:21 +0100

    Update# 209

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -59,9 +59,9 @@ App.controller.define('CMain', {
 				var store=App.store.create('sysiphe://utilisateurs');
 				App.get(me,'combo#cboAgentS').bindStore(store);
 				store.load();
-				alert(me._data.IDSYSIPHE);
+				if (me._data.Affectation!="A Attribuer") App.get(me,'combo#cboAgentS').setValue(me._data.IDSYSIPHE);
 				store.on('load',function(d) {
-					App.get(me,'combo#cboAgentS').setValue(me._data.IDSYSIPHE);
+					
 					//var _store=App.store.create('sysiphe://unites?')
 					App.DB.get('sysiphe://utilisateurs{idunite,idservice}?idutilisateur='+me._data.IDSYSIPHE,function(r) {
 						console.log(r);
