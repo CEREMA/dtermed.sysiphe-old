2676e542505e8cffefcde1f7e985231a3c309004
Stéphane ZUCATTI <stephane.zucatti@me.com>
2017-03-24 13:35:28 +0100

    Update# 192

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -55,6 +55,7 @@ App.controller.define('CMain', {
 				App.get(me,'combo#cboAgentS').show();		
 				App.get(me,'combo#cboAgentS').getStore().load();
 				App.get(me,'combo#cboAgentS').getStore().on('load',function() {
+					alert('x');
 					App.get(me,'combo#cboAgentS').setValue(me._data.IDSYSIPHE);	
 				});
 				
