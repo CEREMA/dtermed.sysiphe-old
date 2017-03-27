71b0249279411bebc72d44faac66aae2c918a6e6
Stéphane ZUCATTI <stephane.zucatti@me.com>
2017-03-27 09:14:48 +0200

    Update# 234

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -81,7 +81,7 @@ App.controller.define('CMain', {
 				store.load();
 				store.on('load',function(s) {
 					App.DB.get('bpclight://agents{Kuni,Ksub}?kage='+me._data.IDUTILISATEUR,function(r) {
-						console.log(r);
+						console.log(r.data[0].Kuni);
 						App.get(me,'combo#cboUnite').setValue(r.data[0].Kuni);
 					});
 					//					
