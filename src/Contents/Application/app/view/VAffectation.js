311a88c0de76045b4e2f80bb397f6a038cf90002
Stéphane ZUCATTI <stephane.zucatti@me.com>
2017-03-28 13:31:18 +0200

    Update# 340

--- src/Contents/Application/app/view/VAffectation.js
+++ src/Contents/Application/app/view/VAffectation.js
@@ -329,9 +329,9 @@ App.view.define('VAffectation',{
         		{boxLabel: 'Oui', name: 'rc', inputValue: '1'},
         		{boxLabel: 'Non', name: 'rc', inputValue: '2', checked: true,margin:{left: 10}},
 				{xtype: "panel",flex: 1,border: false},
-				{boxLabel: 'Réforme', name: 'reforme',xtype: "checkboxfield"},
+				{boxLabel: 'Réforme', name: 'reforme',xtype: "checkboxfield",flex: 1},
 				{
-					xtype: "datefield",
+					xtype: "datefield"
 
 				}					
 				]
