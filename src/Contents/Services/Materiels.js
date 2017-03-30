ae22cd60b0a2454b160d72e5c1daf7681b634ec9
Stéphane ZUCATTI <stephane.zucatti@me.com>
2017-03-30 11:36:27 +0200

    Update# 398

--- src/Contents/Services/Materiels.js
+++ src/Contents/Services/Materiels.js
@@ -34,7 +34,7 @@ Materiels = {
 			'affectations.IDUTILISATEUR',
 			'affectations.DATEENTREE',
 			'affectations.DATESORTIE',
-			'IFNULL(COALESCE(concat(bpclight_agents.nom,' ',bpclight_agents.prenom),utilisateurs.NOMUTILISATEUR),"A ATTRIBUER") Affectation',
+			'IFNULL(COALESCE(concat(bpclight_agents.nom," ",bpclight_agents.prenom),utilisateurs.NOMUTILISATEUR),"A ATTRIBUER") Affectation',
 			'IFNULL(COALESCE(bpclight_unites.libunic,unites.UNITE),"A ATTRIBUER") Unite',
 			'IFNULL(bpclight_subdis.libsubc,"-") Service',
 			'bpclight_etablissements.libets Etablissement'
