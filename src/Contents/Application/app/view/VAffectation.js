e3ad7729660ca48c723fedd104ad2e8c0b0386d7
Stéphane ZUCATTI <stephane.zucatti@me.com>
2017-07-11 09:55:13 +0200

    Update# 489

--- src/Contents/Application/app/view/VAffectation.js
+++ src/Contents/Application/app/view/VAffectation.js
@@ -372,6 +372,7 @@ App.view.define('VAffectation',{
 					flex: 1,
 					itemId: "cboMarque",
 					editable: true,
+					forceSelection: false,
 					enableKeyEvents: true,
 					displayField: "MARQUE",
 					valueField: "IDMARQUE",				
