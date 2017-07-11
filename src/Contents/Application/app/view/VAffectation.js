6068e4294823da0c45a9b283d8ac6052b3479770
Stéphane ZUCATTI <stephane.zucatti@me.com>
2017-07-11 11:31:49 +0200

    Update# 513

--- src/Contents/Application/app/view/VAffectation.js
+++ src/Contents/Application/app/view/VAffectation.js
@@ -347,6 +347,11 @@ App.view.define('VAffectation',{
 					flex: 1
 				},
 				{
+					xtype:"button",
+					width: 12,
+					value: "+"
+				},
+				{
 					xtype: "textfield",
 					fieldLabel: "Bon de commande",
 					bindTo: "NOCOMMANDE",
