547693d70571f907e42509720ecb283a38c13274
Stéphane ZUCATTI <stephane.zucatti@me.com>
2017-07-11 15:45:58 +0200

    Update# 542

--- src/Contents/Application/app/view/VEdition.js
+++ src/Contents/Application/app/view/VEdition.js
@@ -17,7 +17,7 @@ App.view.define('VEdition',{
 		
 		this.title = "Edition";
 		
-		//this.plugins = [rowEditing];
+		this.plugins = [rowEditing];
 		
 		this.items = [
 		{
@@ -27,7 +27,7 @@ App.view.define('VEdition',{
 				text: "Fournisseur",
 				dataIndex: "FOURNISSEUR",
 				editor: {
-					allowBlank:false
+					allowBlank:false	
 				}
 			}],
 			store: App.store.create("sysiphe://fournisseurs",{autoLoad:true})
